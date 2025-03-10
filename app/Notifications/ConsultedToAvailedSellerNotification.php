e3595091d4bf2fd24015ff22b6f06f4c65fa870f
jn-devops <devops@joy-nostalg.com>
2025-03-10 11:06:32 +0800

    bring back the methods

--- app/Notifications/ConsultedToAvailedSellerNotification.php
+++ app/Notifications/ConsultedToAvailedSellerNotification.php
@@ -8,10 +8,9 @@ use Homeful\Contacts\Classes\ContactMetaData;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Homeful\References\Models\Reference;
+use Homeful\Properties\Data\ProjectData;
 use Homeful\Properties\Models\Project;
 use Illuminate\Bus\Queueable;
-use Homeful\Properties\Data\ProjectData;
-
 
 class ConsultedToAvailedSellerNotification extends Notification
 {
@@ -41,17 +40,17 @@ class ConsultedToAvailedSellerNotification extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-//        $name = '';
-//        $contact = $this->getContact();
-//        if ($contact instanceof ContactMetaData) {
-//            $name = $contact->name;
-//        }
-//
-//        $project_name = '';
-//        $project = $this->getProject();
-//        if ($project instanceof ProjectData) {
-//            $project_name = $project->name;
-//        }
+        $name = '';
+        $contact = $this->getContact();
+        if ($contact instanceof ContactMetaData) {
+            $name = $contact->name;
+        }
+
+        $project_name = '';
+        $project = $this->getProject();
+        if ($project instanceof ProjectData) {
+            $project_name = $project->name;
+        }
 
         return (new MailMessage)
                     ->line('The introduction to the notification.')
@@ -71,20 +70,20 @@ class ConsultedToAvailedSellerNotification extends Notification
         ];
     }
 
-//    protected function getContact(): ?ContactMetaData
-//    {
-//        $reference = Reference::where('code', $this->reference_code)->first();
-//        $contact = $reference->getContact();
-//
-//        return $contact instanceof Contact
-//            ? $contact->getData()
-//            : null;
-//    }
-//
-//    protected function getProject(): ?ProjectData
-//    {
-//        $project = Project::where('code', $this->project_code)->first();
-//
-//        return $project instanceof Project ? ProjectData::fromModel($project) : null;
-//    }
+    protected function getContact(): ?ContactMetaData
+    {
+        $reference = Reference::where('code', $this->reference_code)->first();
+        $contact = $reference->getContact();
+
+        return $contact instanceof Contact
+            ? $contact->getData()
+            : null;
+    }
+
+    protected function getProject(): ?ProjectData
+    {
+        $project = Project::where('code', $this->project_code)->first();
+
+        return $project instanceof Project ? ProjectData::fromModel($project) : null;
+    }
 }
