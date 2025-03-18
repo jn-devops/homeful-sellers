fd843503e88f43aba248f134973cd16ca21dbd98
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-03-18 16:46:29 +0800

    fix project_code on null

--- app/Http/Controllers/RedeemController.php
+++ app/Http/Controllers/RedeemController.php
@@ -12,12 +12,21 @@ class RedeemController extends Controller
 {
     public function __invoke(Reference $voucher, Project|string $project = null): array|false
     {
-        RedeemVoucherCode::run($voucher);
+        $project = Project::where('code',$project)->first();
+        RedeemVoucherCode::run($voucher,[
+            "project"=>$project->toArray(),
+            "project_code"=>$project->code
+        ]);
+
         $voucher->refresh();
+        $voucher->save();
+
         $user = $voucher->owner;
+
         $seller_commission_code = $user instanceof User
             ? GetSellerCommissionCode::run($user, $project)
             : null;
+
 
         return $voucher->isRedeemed()
             ? ['seller_commission_code' => $seller_commission_code]
--- app/Observers/ReferenceObserver.php
+++ app/Observers/ReferenceObserver.php
@@ -22,17 +22,25 @@ class ReferenceObserver
         $user = $reference->owner;
         $contact = $reference->getContact();
 
+
+
         /** The following lines forms part of the algorithm */
         /** to get the project code and ultimately  */
         /** the seller commission code. */
         $a = Arr::get($reference->metadata, 'project.code');
         $b = null;
         if ($reference->redeemers) {
-            if (isset($reference->redeemers->metadata)) {
-                $b = Arr::get($reference->redeemers?->metadata, 'project_code');
+            foreach ($reference->redeemers as $redeemer) {
+                if (isset($redeemer->metadata)) {
+                    $b = Arr::get($redeemer?->metadata, 'project_code');
+                }
             }
         }
         $project_code = is_null($b) ? $a : (($a == $b) ? $b : null);
+        $project_code= $b??$a;
+
+        logger($a);
+        logger($project_code);
 
         /** This is main reason for this listener, to attach the seller commission code to the contact */
         $contact->reference_code = GetSellerCommissionCode::run($user, $project_code);
