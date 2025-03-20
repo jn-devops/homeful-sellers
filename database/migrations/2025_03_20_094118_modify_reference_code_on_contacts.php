dd2d160a883e2a6d8408441a1bdfcc18d0fe44e2
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-03-20 20:35:43 +0800

    make the reference code not unique in contacts

---
+++ database/migrations/2025_03_20_094118_modify_reference_code_on_contacts.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropUnique('contacts_reference_code_unique');
+            $table->string('reference_code')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->unique('reference_code');
+        });
+    }
+};
