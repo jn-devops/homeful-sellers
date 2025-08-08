cca4aa4708ef0bf370d1a40f6c2a837ccec31db6
Gari Vivar <garivivar@RLIs-MacBook-Air.local>
2025-08-08 18:10:28 +0800

    added config for LPW

--- config/homeful-sellers.php
+++ config/homeful-sellers.php
@@ -5,7 +5,8 @@ return [
         'customer' => env('CUSTOMER_END_POINT','https://contacts-staging.homeful.ph/api/references/:contact_reference_code'),
         'projects' => env('PROJECTS_END_POINT','https://properties-staging.homeful.ph/fetch-projects'),
         'contact' => env('CONTACT_END_POINT','https://contacts-staging.homeful.ph/'),
-        'contract' => env('CONTRACT_END_POINT','https://contracts-staging.homeful.ph/')
+        'contract' => env('CONTRACT_END_POINT','https://contracts-staging.homeful.ph/'),
+        'loan_processing' => env('LOAN_PROCESS_END_POINT','https://gnc-lazarus-demo.homeful.ph/')
     ],
     'keys'=>[
         'contact_key' =>env('CONTACT_API_KEY')
@@ -20,6 +21,7 @@ return [
         'match' => env('MATCH_END_POINT','https://match.homeful.ph')
     ],
     'api_token' => 'Bearer '.env('API_TOKEN','UcQzIoYSM7OU2Ltrk73fmguiK0k39JW09hmzVuvDBysDAaDklsVmqkBDdYWHP9jD'),
+    'api_loan_process' => 'Bearer '.env('API_TOKEN_LOAN_PROCESS','31|nq1LQLP7oRh4mldNtgUIZQdIXE7t30iQkflHAh3T68e346a5'),
     'encrypt_key' => env('ENCRYPT_KEY',"LoPGxhMbXfq1uUxK2+bn9QbdvTheCKJRxLKWS1qUIFtQ+l+mSPPzLeyMMHVwS5YO")
     
 ];
